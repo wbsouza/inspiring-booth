ddb87f30ca29428e657dec6cb78f2222572e849e
Wellington Souza <wellington@wisecoding.io>
2021-09-19 18:51:30 -0700

    Improving the build pipeline

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -x
+
+COMMIT_HASH=$1
+if [ "${COMMIT_HASH}" == "" ]; then
+  COMMIT_HASH=$(git show -s --format=%H)
+  echo "Using COMMIT_HASH from the git command => ${COMMIT_HASH}"
+else
+  echo "Using COMMIT_HASH from the command-line argument => ${COMMIT_HASH}"
+fi
+
+if [ -e "./src/index.html" ]; then
+  sed -i 's:COMMIT_HASH:'${COMMIT_HASH}':g' index.html
+  surge --project ./src --domain "${SURGE_APP_NAME:-inspiring-booth}.surge.sh"
+  exit 0
+else
+  echo "The file src/index.html does not exist!"
+  exit 1
+fi
